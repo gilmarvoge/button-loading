12eb96c8a86d239ead6102b5edd081e79b70c786
gilmarvogel <gilmarvogel@gmail.com>
2021-01-11 14:31:03 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ButtonLoading from './lib/ButtonLoading'
+
+ReactDOM.render(
+  <div>
+    <ButtonLoading />   
+  </div>,
+  document.getElementById('root')
+);
